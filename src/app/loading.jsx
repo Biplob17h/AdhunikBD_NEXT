fefbc16e1cad8735af624b2177ae54f67e741107
Al Amin <alaminpgmr@gmail.com>
2025-01-11 18:36:26 +0600

    add Loading component; implement spinner for loading state

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div class="h-10 w-10 animate-spin rounded-full border-4 border-secondary border-t-primary" />
+    </div>
+  );
+};
+
+export default Loading;
