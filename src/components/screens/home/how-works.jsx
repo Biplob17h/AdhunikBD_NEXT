df0e1322cdcdb5cf96b54833c48e3dff6b99b935
Al Amin <alaminpgmr@gmail.com>
2025-01-12 14:05:57 +0600

    refactor Recommended and HowWork sections; enhance grid responsiveness and adjust layout styles

--- src/components/screens/home/how-works.jsx
+++ src/components/screens/home/how-works.jsx
@@ -15,7 +15,7 @@ const HowWorkSection = () => {
           </div>
 
           {/* Step Cards */}
-          <div className="mx-auto mt-10 grid grid-cols-1 gap-8 px-6 md:grid-cols-2 md:px-8 lg:gap-x-20 lg:gap-y-0 xl:max-w-[1021px] xl:px-0">
+          <div className="mx-auto mt-10 grid grid-cols-1 gap-8 px-6 sm:grid-cols-2 md:px-8 lg:gap-x-20 lg:gap-y-0 xl:max-w-[1021px] xl:px-0">
             {/* Step 1 */}
             <div className="max-w-[426px] space-y-4 rounded-3xl border border-[#E6F1FE] bg-white p-4 text-center lg:p-8">
               <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full border-[6px] bg-black font-inter text-[32px] leading-none text-white">
@@ -63,7 +63,7 @@ const HowWorkSection = () => {
             <div className="hidden lg:block" />
 
             {/* Step 4 */}
-            <div className="max-w-[426px] space-y-4 rounded-3xl border border-[#E6F1FE] bg-white p-4 text-center lg:p-8">
+            <div className="relative max-w-[426px] space-y-4 rounded-3xl border border-[#E6F1FE] bg-white p-4 text-center lg:-top-12 lg:p-8 xl:-top-[128px] xl:left-10">
               <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full border-[6px] bg-black font-inter text-[32px] leading-none text-white">
                 4
               </div>
