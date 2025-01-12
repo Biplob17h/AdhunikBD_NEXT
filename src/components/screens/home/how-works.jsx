627e3085918fd176ce210df1a6155fe0f7cec7f9
Al Amin <alaminpgmr@gmail.com>
2025-01-12 13:10:37 +0600

    refactor HowWorkSection layout; enhance step card design and responsiveness

--- src/components/screens/home/how-works.jsx
+++ src/components/screens/home/how-works.jsx
@@ -2,7 +2,7 @@ const HowWorkSection = () => {
   return (
     <section className="pb-16 xl:pb-32">
       <div className="how-works-bg py-10 xl:py-20">
-        <div className="container_fluid">
+        <div className="container mx-auto">
           <div className="heading mx-auto max-w-[855px] space-y-4 text-center">
             <h2 className="text-3xl font-bold uppercase text-black/75 md:text-5xl">
               How It Works
@@ -14,8 +14,68 @@ const HowWorkSection = () => {
             </p>
           </div>
 
-          {/* Dynamic categories */}
-          <div className="mt-10">cards</div>
+          {/* Step Cards */}
+          <div className="mx-auto mt-10 grid grid-cols-1 gap-8 px-6 md:grid-cols-2 md:px-8 lg:gap-x-20 lg:gap-y-0 xl:max-w-[1021px] xl:px-0">
+            {/* Step 1 */}
+            <div className="max-w-[426px] space-y-4 rounded-3xl border border-[#E6F1FE] bg-white p-4 text-center lg:p-8">
+              <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full border-[6px] bg-black font-inter text-[32px] leading-none text-white">
+                1
+              </div>
+              <h3 className="text-2xl font-semibold uppercase text-black/75">
+                Lorem Ipsum
+              </h3>
+              <p className="max-w-[340px] text-base font-light leading-[27px] text-black/60">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration
+              </p>
+            </div>
+            <div className="hidden lg:block" />
+            <div className="hidden lg:block" />
+
+            {/* Step 2 */}
+            <div className="max-w-[426px] space-y-4 rounded-3xl border border-[#E6F1FE] bg-white p-4 text-center lg:relative lg:p-8 xl:-top-24 xl:left-10">
+              <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full border-[6px] bg-black font-inter text-[32px] leading-none text-white">
+                2
+              </div>
+              <h3 className="text-2xl font-semibold uppercase text-black/75">
+                Lorem Ipsum
+              </h3>
+              <p className="max-w-[340px] text-base font-light leading-[27px] text-black/60">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration
+              </p>
+            </div>
+
+            {/* Step 3 */}
+            <div className="max-w-[426px] space-y-4 rounded-3xl border border-[#E6F1FE] bg-white p-4 text-center lg:relative lg:p-8 xl:-top-[72px] xl:left-20">
+              <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full border-[6px] bg-black font-inter text-[32px] leading-none text-white">
+                3
+              </div>
+              <h3 className="text-2xl font-semibold uppercase text-black/75">
+                Lorem Ipsum
+              </h3>
+              <p className="max-w-[340px] text-base font-light leading-[27px] text-black/60">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration
+              </p>
+            </div>
+            <div className="hidden lg:block" />
+            <div className="hidden lg:block" />
+
+            {/* Step 4 */}
+            <div className="max-w-[426px] space-y-4 rounded-3xl border border-[#E6F1FE] bg-white p-4 text-center lg:p-8">
+              <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full border-[6px] bg-black font-inter text-[32px] leading-none text-white">
+                4
+              </div>
+              <h3 className="text-2xl font-semibold uppercase text-black/75">
+                Lorem Ipsum
+              </h3>
+              <p className="max-w-[340px] text-base font-light leading-[27px] text-black/60">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
