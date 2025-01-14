2ad7c2a8f4aae3f6d4bb62b431d9ec958105d4ea
Al Amin <alaminpgmr@gmail.com>
2025-01-14 17:57:51 +0600

    feat(BlogDetailsPage): update layout to display a "Coming soon..." message

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,5 +1,9 @@
 const BlogDetailsPage = () => {
-  return <div>BlogDetailsPage</div>;
+  return (
+    <div className="flex h-screen items-center justify-center bg-secondary text-5xl text-black/60">
+      Coming soon...
+    </div>
+  );
 };
 
 export default BlogDetailsPage;
