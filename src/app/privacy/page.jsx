33d986351ddc6ce6d0d2dbf0fca6fefd6cc00d1d
Al Amin <alaminpgmr@gmail.com>
2025-01-14 17:10:56 +0600

    feat(PrivacyPage): add AdsBannerSection to privacy policy page

--- src/app/privacy/page.jsx
+++ src/app/privacy/page.jsx
@@ -1,3 +1,4 @@
+import AdsBannerSection from "@/components/screens/home/app-ads";
 import PrivacySection from "@/components/screens/privacy/privacy";
 import HeaderSection from "@/components/shared/header";
 
@@ -9,6 +10,7 @@ const PrivacyPage = () => {
         links={["Home", "Privacy Policy"]}
       />
       <PrivacySection />
+      <AdsBannerSection />
     </>
   );
 };
