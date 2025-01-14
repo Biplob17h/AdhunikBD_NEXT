1f63cf77e175e433a603f5f5a0673a4cf826b7e4
Al Amin <alaminpgmr@gmail.com>
2025-01-14 11:55:31 +0600

    feat(DetailTabSection): implement tabbed interface with details, service overview, FAQs, and reviews sections

--- src/components/screens/category-details/detail-tabs.jsx
+++ src/components/screens/category-details/detail-tabs.jsx
@@ -1,9 +1,36 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import DetailSection from "./details";
+import FaqDetailSection from "./faq-details";
+import ReviewDetailSection from "./reviews-detail";
+import ServiceOverviewSection from "./service-overview";
+
 const DetailTabSection = () => {
-  return <section className="">
-    <div>
-        
-    </div>
-  </section>;
+  return (
+    <section className="pb-16 xl:pb-32">
+      <div className="container_fluid">
+        <Tabs defaultValue="details">
+          <TabsList className="mb-4 w-full justify-normal gap-4 rounded-none border-b bg-transparent pb-6 shadow-none lg:gap-14">
+            <TabsTrigger value="details">Details</TabsTrigger>
+            <TabsTrigger value="service-overview">Service Overview</TabsTrigger>
+            <TabsTrigger value="faqs">FAQ</TabsTrigger>
+            <TabsTrigger value="reviews">Reviews</TabsTrigger>
+          </TabsList>
+          <TabsContent value="details">
+            <DetailSection />
+          </TabsContent>{" "}
+          <TabsContent value="service-overview">
+            <ServiceOverviewSection />
+          </TabsContent>
+          <TabsContent value="faqs">
+            <FaqDetailSection />
+          </TabsContent>{" "}
+          <TabsContent value="reviews">
+            <ReviewDetailSection />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </section>
+  );
 };
 
 export default DetailTabSection;
