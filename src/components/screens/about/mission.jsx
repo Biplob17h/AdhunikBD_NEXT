ae07e1b85974b25cdf31868329ad3dac47fe836b
Al Amin <alaminpgmr@gmail.com>
2025-01-15 12:36:07 +0600

    feat(MissionSection): add animations to enhance visual engagement of mission and vision content

--- src/components/screens/about/mission.jsx
+++ src/components/screens/about/mission.jsx
@@ -1,21 +1,38 @@
+"use client";
+import { motion } from "framer-motion";
+
 const OurMissionSection = () => {
   return (
     <section className="mb-16 bg-secondary py-8 xl:py-16">
       <div className="container_fluid">
         <div className="text-center">
-          <h2 className="text-3xl font-bold text-black/75 md:text-5xl">
+          <motion.h2
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            className="text-3xl font-bold text-black/75 md:text-5xl"
+          >
             Our Mission
-          </h2>
+          </motion.h2>
         </div>
         <div className="mx-auto mt-10 grid max-w-[956px] grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-          <div>
+          <motion.div
+            initial={{ opacity: 0, y: -80 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.1, ease: "easeInOut" }}
+          >
             <img
               src="/images/about/vision.png"
               className="w-full rounded-2xl object-cover"
               alt="vision"
             />
-          </div>
-          <div className="space-y-20 rounded-2xl bg-white p-6">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: -80 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            className="space-y-20 rounded-2xl bg-white p-6"
+          >
             <h6 className="inline-flex items-center gap-2 text-xl font-medium text-black">
               {" "}
               <span className="text-[10px]">⚫</span> Our Mission
@@ -24,15 +41,24 @@ const OurMissionSection = () => {
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the hjk lk ;k yyt{" "}
             </p>
-          </div>
-          <div>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: -80 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.1, ease: "easeInOut" }}
+          >
             <img
               src="/images/about/mission.png"
               className="w-full rounded-2xl object-cover"
               alt="vision"
             />
-          </div>
-          <div className="space-y-20 rounded-2xl bg-white p-6">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: -80 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.1, ease: "easeInOut" }}
+            className="space-y-20 rounded-2xl bg-white p-6"
+          >
             <h6 className="inline-flex items-center gap-2 text-xl font-medium text-black">
               {" "}
               <span className="text-[10px]">⚫</span> Our Vision
@@ -41,15 +67,24 @@ const OurMissionSection = () => {
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the hjk lk ;k yyt{" "}
             </p>
-          </div>
-          <div>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 80 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.2, ease: "easeInOut" }}
+          >
             <img
               src="/images/about/improvement.png"
               className="w-full rounded-2xl object-cover"
               alt="vision"
             />
-          </div>
-          <div className="space-y-20 rounded-2xl bg-white p-6">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: 80 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.3, ease: "easeInOut" }}
+            className="space-y-20 rounded-2xl bg-white p-6"
+          >
             <h6 className="inline-flex items-center gap-2 text-xl font-medium text-black">
               {" "}
               <span className="text-[10px]">⚫</span> Our Improvement
@@ -58,7 +93,7 @@ const OurMissionSection = () => {
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the hjk lk ;k yyt{" "}
             </p>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
