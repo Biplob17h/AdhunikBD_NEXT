2c268daa78f6909a7e18bdb3a1b6113e9c0664f3
Al Amin <alaminpgmr@gmail.com>
2025-01-15 11:23:30 +0600

    feat(CategoriesSection): add animations to category headings and links for enhanced visual appeal

--- src/components/screens/home/categories.jsx
+++ src/components/screens/home/categories.jsx
@@ -1,4 +1,6 @@
+"use client";
 import { categories } from "@/data/categories.data";
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 const CategoriesSection = () => {
@@ -6,25 +8,40 @@ const CategoriesSection = () => {
     <section className="py-16 xl:py-32">
       <div className="container_fluid">
         <div className="heading mx-auto max-w-[855px] space-y-4 text-center">
-          <h2 className="text-3xl font-bold text-black/75 md:text-5xl">
+          <motion.h2
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeInOut" }}
+            className="text-3xl font-bold text-black/75 md:text-5xl"
+          >
             Explore Our Categories
-          </h2>
-          <p className="text-base text-black/60">
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeInOut" }}
+            className="text-base text-black/60"
+          >
             Discover tailored categories designed to simplify your search,
             explore diverse options, and <br className="hidden md:block" /> find
             exactly what you need effortlessly
-          </p>
+          </motion.p>
         </div>
 
         {/* Dynamic categories */}
         <div className="mt-10 grid grid-cols-2 gap-3 md:grid-cols-4 md:gap-4 xl:grid-cols-6 xl:gap-6">
-          {categories.map((category) => (
+          {categories.map((category, index) => (
             <Link
               key={category.id}
               href={`/categories/${category.id}`}
               className="bg-categories-hover group rounded p-[1.15px]"
             >
-              <div className="space-y-4 rounded bg-white p-4 text-center shadow-4xl">
+              <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8 + index * 0.1, ease: "easeInOut" }}
+                className="space-y-4 rounded bg-white p-4 text-center shadow-4xl"
+              >
                 <div className="mx-auto h-[67.37px] w-[67.37px]">
                   <img
                     src={category.image}
@@ -38,18 +55,23 @@ const CategoriesSection = () => {
                 <p className="text-base font-light text-[#a1a1a1]">
                   {category.description}
                 </p>
-              </div>
+              </motion.div>
             </Link>
           ))}
         </div>
-        <div className="mt-10 text-center">
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+          className="mt-10 text-center"
+        >
           <Link
             href="/categories"
             className="rounded-lg bg-black px-8 py-3 font-inter text-lg text-white transition-all duration-300 ease-in-out hover:bg-primary"
           >
             View More
           </Link>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
